ad3867bb23fcdda510d6ca98d965221d20f3ab88
kxu <kxu@redhat.com>
2020-06-08 11:17:21 -0400

    Removing starting agent feature from overview

--- org.openjdk.jmc.console.ext.agent/src/main/java/org/openjdk/jmc/console/ext/agent/tabs/overview/OverviewTab.java
+++ org.openjdk.jmc.console.ext.agent/src/main/java/org/openjdk/jmc/console/ext/agent/tabs/overview/OverviewTab.java
@@ -23,7 +23,6 @@ import com.sun.tools.attach.VirtualMachine;
 public class OverviewTab extends AgentFormPage {
 	private static final String ID = "org.openjdk.jmc.console.ext.agent.tabs.overview.OverviewTab";
 	private static final String TITLE = "Overview";
-	private LoadAgentSection loadAgentSection;
 	private VirtualMachine vm;
 	private AgentJmxHelper agentJMXHelper;
 	private Composite container;
@@ -34,45 +33,19 @@ public class OverviewTab extends AgentFormPage {
 	}
 
 	@Inject
-	protected void createPageContent(IManagedForm managedForm, AgentJmxHelper agentJMXHelper, IConnectionHandle connectionHandle) {
+	protected void createPageContent(IManagedForm managedForm, AgentJmxHelper agentJMXHelper) {
 		this.agentJMXHelper = agentJMXHelper;
-		
+
 		ScrolledForm form = managedForm.getForm();
 
 		this.container = form.getBody();
 		container.setLayout(MCLayoutFactory.createFormPageLayout());
 
-		if (!agentJMXHelper.isMXBeanRegistered() && agentJMXHelper.isLocalJvm()) {
-			String pid = connectionHandle.getServerDescriptor().getJvmInfo().getPid().toString();
-			vm = initVM(pid);
-			loadAgentSection = new LoadAgentSection(container, vm);
-			loadAgentSection.setLoadAgentListener(this::loadAgentListener);
-			return;
-		}
-		
-		if (!agentJMXHelper.isMXBeanRegistered() && !agentJMXHelper.isLocalJvm()) {
-			Label l = new Label(container, SWT.NONE);
-			l.setText("Starting agent on remote JVM is not supported");
-			return;
-		}
-
 		Label l = new Label(container, SWT.NONE);
 		l.setText("Agent is loaded");
 	}
 
-	private VirtualMachine initVM(String pid) {
-		VirtualMachine vm = null;
-		try {
-			vm = VirtualMachine.attach(pid);
-		} catch (AttachNotSupportedException | IOException e) {
-			AgentPlugin.getDefault().getLogger().log(Level.SEVERE,
-					"Could not attach process with pid " + pid + " and create a VirtualMachine", e);
-		}
-		return vm;
-	}
-
 	private void loadAgentListener() {
-		loadAgentSection.dispose();
 		Label l = new Label(container, SWT.NONE);
 		l.setText("Agent is loaded");
 		container.layout();
