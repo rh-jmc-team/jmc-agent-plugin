be762da67f8413115bbbd24c7ff634aa85655c6b
kxu <kxu@redhat.com>
2020-06-11 18:54:33 -0400

    make maven less verbose when building jmc

--- scripts/buildjmc.sh
+++ scripts/buildjmc.sh
@@ -7,21 +7,21 @@ sh -c "git clone --depth 1 https://github.com/openjdk/jmc.git"
 
 echo "======== Building p2 repo ==================="
 cd jmc/releng/third-party
-sh -c "mvn p2:site"
+sh -c "mvn --quiet p2:site"
 
 echo "======== Starting p2 repo ==================="
-sh -c "nohup mvn jetty:run &"
+sh -c "nohup mvn --quiet jetty:run &"
 
 cd ../../core
 echo "======== Installing core ===================="
-sh -c "mvn install"
+sh -c "mvn --quiet install"
 
 echo "======== Installing application ============="
 cd ..
-sh -c "mvn install"
+sh -c "mvn --quiet install"
 
 echo "======== Installing jmc ====================="
 cd agent
-sh -c "mvn install"
+sh -c "mvn --quiet install"
 
 echo "======== Finished building JMC =============="
