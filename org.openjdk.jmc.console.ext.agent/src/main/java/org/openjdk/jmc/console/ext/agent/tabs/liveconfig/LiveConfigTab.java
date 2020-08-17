cd75160544f5919fab9e4ffbee7ac64485921370
Jessye Coleman-Shapiro <cjessye@gmail.com>
2020-08-17 15:28:47 -0400

    Live config buttons (#74)
    * Add live config action buttons
    
    * make spotbugs happy

--- org.openjdk.jmc.console.ext.agent/src/main/java/org/openjdk/jmc/console/ext/agent/tabs/liveconfig/LiveConfigTab.java
+++ org.openjdk.jmc.console.ext.agent/src/main/java/org/openjdk/jmc/console/ext/agent/tabs/liveconfig/LiveConfigTab.java
@@ -1,9 +1,44 @@
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, Red Hat Inc. All rights reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The contents of this file are subject to the terms of either the Universal Permissive License
+ * v 1.0 as shown at http://oss.oracle.com/licenses/upl
+ *
+ * or the following license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided with
+ * the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.openjdk.jmc.console.ext.agent.tabs.liveconfig;
 
 import javax.inject.Inject;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.widgets.FormToolkit;
@@ -11,6 +46,8 @@ import org.eclipse.ui.forms.widgets.ScrolledForm;
 import org.openjdk.jmc.console.ext.agent.AgentJmxHelper;
 import org.openjdk.jmc.console.ext.agent.editor.AgentEditor;
 import org.openjdk.jmc.console.ext.agent.editor.AgentFormPage;
+import org.openjdk.jmc.console.ext.agent.manager.model.PresetRepository;
+import org.openjdk.jmc.console.ext.agent.manager.model.PresetRepositoryFactory;
 import org.openjdk.jmc.rjmx.IConnectionHandle;
 import org.openjdk.jmc.ui.misc.MCLayoutFactory;
 
@@ -21,6 +58,7 @@ public class LiveConfigTab extends AgentFormPage {
 	private SashForm sashForm;
 	private EventTreeSection eventTree;
 	private FeatureTableSection eventTable;
+	private final PresetRepository presetRepository = PresetRepositoryFactory.createSingleton();
 
 	public LiveConfigTab(AgentEditor editor) {
 		super(editor, ID, TITLE);
@@ -34,7 +72,11 @@ public class LiveConfigTab extends AgentFormPage {
 		Composite body = form.getBody();
 		body.setLayout(MCLayoutFactory.createFormPageLayout());
 
-		sashForm = new SashForm(body, SWT.HORIZONTAL);
+		Composite pageContainer = new Composite(body, SWT.NONE);
+		pageContainer.setLayout(new GridLayout(2, false));
+		pageContainer.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+		sashForm = new SashForm(pageContainer, SWT.HORIZONTAL);
 		sashForm.setLayoutData(MCLayoutFactory.createFormPageLayoutData());
 		toolkit.adapt(sashForm, false, false);
 
@@ -46,6 +88,7 @@ public class LiveConfigTab extends AgentFormPage {
 		eventTree.selectTopEvent();
 		sashForm.setWeights(new int[] {3, 4});
 
+		new ActionButtons(pageContainer, presetRepository, helper, eventTree);
 	}
 
 }
